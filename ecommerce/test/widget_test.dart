6773b22acc079cf8fdfd0b06e9b31c30ecd06f40
Umar Mahamud <umahamud9@gmail.com>
2025-12-09 20:51:32 +0300

    Add initial structure for AddScreen, EditScreen, HomeScreen, ProductScreen, and ProductCard; update main.dart for routing

--- ecommerce/test/widget_test.dart
+++ ecommerce/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:layout/main.dart';
+import 'package:layout/features/presentation/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
